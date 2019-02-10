54c8d4e2f954304f7ba377c1ca27b71580ca65a0
navammanuel <nava.m.manuel@gmail.com>
2019-02-09 17:38:54 -0800

    Removed console.log from the ngOnInit method
    Removed console.log from the ngOnInit method

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -15,8 +15,5 @@ export class ProductDescriptionComponent implements OnInit {
   ngOnInit() {
     this._productService.getAlbum(1).subscribe(response =>
     this.albumInfo = response);
-
-    console.log(this.albumInfo);
-
   }
 }
